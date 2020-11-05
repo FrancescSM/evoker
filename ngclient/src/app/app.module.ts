74501a64fb57e153e229ca749b56ed13e46d67b0
Francesc Costa <francescc@hp.com>
2020-11-05 07:42:17 +0100

    Add file explorer

--- ngclient/src/app/app.module.ts
+++ ngclient/src/app/app.module.ts
@@ -5,18 +5,53 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { RendererComponent } from './renderer/renderer.component';
 import { VisElementComponent } from './vis-element/vis-element.component';
+import { FileExplorerComponent } from './file-explorer/file-explorer.component';
+
+import { CommonModule } from '@angular/common'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatIconModule } from '@angular/material/icon'
+import { MatGridListModule } from '@angular/material/grid-list'
+import { MatMenuModule } from '@angular/material/menu'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatInputModule } from '@angular/material/input'
+import { MatButtonModule } from '@angular/material/button'
+import { FormsModule } from '@angular/forms';
+import { NewFolderDialogComponent } from './file-explorer/modals/new-folder-dialog/new-folder-dialog.component';
+import { RenameDialogComponent } from './file-explorer/modals/rename-dialog/rename-dialog.component';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     RendererComponent,
-    VisElementComponent
+    VisElementComponent,
+    FileExplorerComponent,
+    NewFolderDialogComponent,
+    RenameDialogComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatGridListModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
--- ngclient/src/app/sidebar/sidebar.component.ts
+++ ngclient/src/app/sidebar/sidebar.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import SmartConnect from 'wslink/src/SmartConnect';
 import vtkImageStream from 'vtk.js/Sources/IO/Core/ImageStream';
 import FileListing from '../protocols/FileListing';
+import { Observable } from 'rxjs'
+import { FileElement } from '../file-explorer/model/file-element'
+import { FileService } from '../service/file-service.service';
 
 const REMOTE_API = {
   // ColorManager,
@@ -28,7 +31,15 @@ const REMOTE_API = {
 })
 export class SidebarComponent implements OnInit {
 
-
+  // id?: string
+  // isFolder: boolean
+  // name: string
+  // parent: string
+  // {id:"id1",isFolder: false,name:"name1",parent:"parent1"}
+  fileElements: Observable<FileElement[]>;
+  currentRoot: FileElement;
+  currentPath: string;
+  canNavigateUp = false;
 
   //private config: any = null;
   private connection: any = null;
@@ -41,27 +52,29 @@ export class SidebarComponent implements OnInit {
   private updateBusy: any;
   private busyCount: number = 0;
 
-  constructor() {
+  constructor(private fileService: FileService) {
+
     this.updateBusy = (delta = 0) => {
       this.busyCount += delta;
-    //   if (this.busyCallback) {
-    //     if (this.timeoutId) {
-    //       clearTimeout(this.timeoutId);
-    //       this.timeoutId = 0;
-    //     }
-    //     if (!this.busyCount) {
-    //       // Try to delay the notification of idle
-    //       this.timeoutId = setTimeout(() => {
-    //         this.notifyBusy();
-    //       }, 50);
-    //     } else {
-    //       this.notifyBusy();
-    //     }
-    //   }
+      //   if (this.busyCallback) {
+      //     if (this.timeoutId) {
+      //       clearTimeout(this.timeoutId);
+      //       this.timeoutId = 0;
+      //     }
+      //     if (!this.busyCount) {
+      //       // Try to delay the notification of idle
+      //       this.timeoutId = setTimeout(() => {
+      //         this.notifyBusy();
+      //       }, 50);
+      //     } else {
+      //       this.notifyBusy();
+      //     }
+      //   }
     };
   }
 
   ngOnInit(): void {
+    this.connect();
   }
 
   connect() {
@@ -74,6 +87,34 @@ export class SidebarComponent implements OnInit {
         // dispatch('PVL_APP_ROUTE_RUN');
         // dispatch('PVL_COLOR_FETCH_PRESET_NAMES', 500);
         // clientToConnect.updateBusy(-1);
+        // this.fileService.add({ isFolder: true, name: 'folder1', parent: this.currentRoot ? this.currentRoot.id : 'root' });
+        // this.fileService.add({ isFolder: true, name: 'folder2', parent: this.currentRoot ? this.currentRoot.id : 'root' });
+        // this.fileService.add({ isFolder: false, name: 'file1', parent: this.currentRoot ? this.currentRoot.id : 'root' });
+        // this.fileService.add({ isFolder: false, name: 'file2', parent: this.currentRoot ? this.currentRoot.id : 'root' });
+
+
+        this.remote.FileListing.listServerDirectory('.')
+          .then((listing) => {
+            const { dirs, files, groups, path } = listing;
+            // console.log('dirs ', dirs, 'files ', files);
+            dirs.forEach(element => {
+              console.log(element);
+              this.fileService.add({ isFolder: true, name: element, parent: this.currentRoot ? this.currentRoot.id : 'root' });
+            });
+            files.forEach(element => {
+              console.log(element);
+              this.fileService.add({ isFolder: false, name: element.label, parent: this.currentRoot ? this.currentRoot.id : 'root' });
+            });
+            this.updateFileElementQuery();
+            // this.files = files;
+            // this.groups = groups;
+            // this.directories = dirs;
+            // this.path = path;
+            // this.label = this.path.slice(-1)[0];
+          })
+          .catch(console.error);
+
+
       })
       .catch((error) => {
         console.log('error');
@@ -170,16 +211,73 @@ export class SidebarComponent implements OnInit {
     console.log('openFile');
 
     this.remote.FileListing.listServerDirectory('.')
-    .then((listing) => {
-      const { dirs, files, groups, path } = listing;
-      console.log('dirs ', dirs, 'files ', files);
-      // this.files = files;
-      // this.groups = groups;
-      // this.directories = dirs;
-      // this.path = path;
-      // this.label = this.path.slice(-1)[0];
-    })
-    .catch(console.error);
+      .then((listing) => {
+        const { dirs, files, groups, path } = listing;
+        console.log('dirs ', dirs, 'files ', files);
+        // this.files = files;
+        // this.groups = groups;
+        // this.directories = dirs;
+        // this.path = path;
+        // this.label = this.path.slice(-1)[0];
+      })
+      .catch(console.error);
     return true;
+  }
+
+  updateFileElementQuery() {
+    this.fileElements = this.fileService.queryInFolder(this.currentRoot ? this.currentRoot.id : 'root');
+  }
+
+  addFolder(folder: { name: string }) {
+    this.fileService.add({ isFolder: true, name: folder.name, parent: this.currentRoot ? this.currentRoot.id : 'root' });
+    this.updateFileElementQuery();
+  }
+
+  removeElement(element: FileElement) {
+    this.fileService.delete(element.id);
+    this.updateFileElementQuery();
+  }
+
+  moveElement(event: { element: FileElement; moveTo: FileElement }) {
+    this.fileService.update(event.element.id, { parent: event.moveTo.id });
+    this.updateFileElementQuery();
+  }
+
+  renameElement(element: FileElement) {
+    this.fileService.update(element.id, { name: element.name });
+    this.updateFileElementQuery();
+  }
+
+  navigateUp() {
+    if (this.currentRoot && this.currentRoot.parent === 'root') {
+      this.currentRoot = null;
+      this.canNavigateUp = false;
+      this.updateFileElementQuery();
+    } else {
+      this.currentRoot = this.fileService.get(this.currentRoot.parent);
+      this.updateFileElementQuery();
+    }
+    this.currentPath = this.popFromPath(this.currentPath);
+  }
+
+  navigateToFolder(element: FileElement) {
+    this.currentRoot = element;
+    this.updateFileElementQuery();
+    this.currentPath = this.pushToPath(this.currentPath, element.name);
+    this.canNavigateUp = true;
+  }
+
+  pushToPath(path: string, folderName: string) {
+    let p = path ? path : '';
+    p += `${folderName}/`;
+    return p;
+  }
+
+  popFromPath(path: string) {
+    let p = path ? path : '';
+    let split = p.split('/');
+    split.splice(split.length - 2, 1);
+    p = split.join('/');
+    return p;
   }
 }
